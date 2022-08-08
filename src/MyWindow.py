1bf374fd8f40a1db306a520f4f0d826f596dae38
Bruno Di Sanzo <bdisanzo@itba.edu.ar>
2022-08-07 21:04:59 -0300

    Inicio de la app

--- src/MyWindow.py
+++ src/MyWindow.py
@@ -2,18 +2,13 @@
 from PyQt5.QtWidgets import QMainWindow
 
 # Project modules
-from src.ui.mainwindow import Ui_Form
+from src.ui.mainwindow import Ui_MainWindow
 
 
-class MyWindow(QMainWindow, Ui_Form):
+class MyWindow(QMainWindow, Ui_MainWindow):
 
     def __init__(self):
         super(MyWindow, self).__init__()
         self.setupUi(self)
-
-    def numeroAlCuadrad0(self, numero):
-        num = numero*numero
-
-        self.label.setText(str(num))
 
 
